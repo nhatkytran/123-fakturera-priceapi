9ee4e0d1745b355ceb3fbd6a61fff9c58071159c
kytran-saritasa <ky.tran@saritasa.com>
2025-09-17 17:34:27 +0700

    Register database

--- src/config/db.js
+++ src/config/db.js
@@ -1,9 +1,20 @@
+import fp from 'fastify-plugin';
 import { Sequelize } from 'sequelize';
 
-/** Setup sequelize instance. */
-export const setupSequelize = fastify => {
-  return new Sequelize(fastify.config.SUPABASE_URL, {
+/** Register database connection. */
+export const registerDb = fp(async fastify => {
+  const sequelize = new Sequelize(fastify.config.SUPABASE_URL, {
     host: 'supabase_host',
     dialect: 'postgres',
   });
-};
+
+  fastify.decorate('sequelize', sequelize);
+
+  try {
+    await sequelize.authenticate();
+    fastify.log.info('Database connected successfully.');
+  } catch (error) {
+    fastify.log.error('Unable to connect to the database:', error);
+    process.exit(1);
+  }
+});
--- src/server.js
+++ src/server.js
@@ -3,14 +3,13 @@ import Fastify from 'fastify';
 import { loggerConfig } from './config/logger.js';
 import { registerEnv } from './config/env.js';
 import { registerCors } from './config/cors.js';
-import { setupSequelize } from './config/db.js';
+import { registerDb } from './config/db.js';
 
 const fastify = Fastify({ logger: loggerConfig() });
 
 await registerEnv(fastify);
 await registerCors(fastify);
-
-const sequelize = setupSequelize(fastify);
+await registerDb(fastify);
 
 fastify.get('/', async (request, reply) => {
   return { hello: 'world' };
@@ -18,8 +17,6 @@ fastify.get('/', async (request, reply) => {
 
 (async () => {
   try {
-    await sequelize.sync({ force: true });
-    fastify.log.info('Database connected successfully.');
     await fastify.listen({ port: fastify.config.PORT });
   } catch (err) {
     fastify.log.error(err);
