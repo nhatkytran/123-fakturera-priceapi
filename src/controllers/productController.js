7ed461bae655e7705a88c7de9bd30583728bc282
kytran-saritasa <ky.tran@saritasa.com>
2025-09-17 18:26:08 +0700

    Improve reply send data

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -4,5 +4,5 @@ import { catchAsync } from '../utils/catchAsync.js';
 export const getAllProducts = Product =>
   catchAsync(async (request, reply) => {
     const products = await Product.findAll();
-    return { status: 'success', results: products.length, data: products };
+    reply.send({ status: 'success', results: products.length, data: products });
   });
