cda8c82ca753be741da59ae09280310b68e0ba9a
kytran-saritasa <ky.tran@saritasa.com>
2025-09-17 18:33:40 +0700

    Handle exceptions

--- src/server.js
+++ src/server.js
@@ -1,5 +1,12 @@
 import Fastify from 'fastify';
 
+/** Handle uncaught exceptions. */
+process.on('uncaughtException', err => {
+  console.error('UNCAUGHT EXCEPTION! Shutting down...');
+  console.error(err.name, err.message);
+  process.exit(1);
+});
+
 import { loggerConfig } from './config/logger.js';
 import { registerEnv } from './config/env.js';
 import { registerCors } from './config/cors.js';
@@ -32,3 +39,30 @@ fastify.setErrorHandler((error, request, reply) => {
     process.exit(1);
   }
 })();
+
+/** Handle unhandled rejections. */
+process.on('unhandledRejection', err => {
+  console.error('UNHANDLED REJECTION! Shutting down...');
+  console.error(err.name, err.message);
+  if (fastify.server) {
+    fastify.server.close(() => {
+      if (fastify.sequelize) {
+        fastify.sequelize.close();
+      }
+      process.exit(1);
+    });
+  }
+});
+
+/** Handle SIGTERM. */
+process.on('SIGTERM', () => {
+  console.error('SIGTERM RECEIVED. Shutting down gracefully.');
+  if (fastify.server) {
+    fastify.server.close(() => {
+      console.error('Process terminated!');
+      if (fastify.sequelize) {
+        fastify.sequelize.close();
+      }
+    });
+  }
+});
