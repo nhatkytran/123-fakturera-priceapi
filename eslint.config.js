0cae48f7455a62865759ca6333fbec6c8dbc352b
kytran-saritasa <ky.tran@saritasa.com>
2025-09-17 11:52:38 +0700

    Add eslint rules

--- eslint.config.js
+++ eslint.config.js
@@ -7,16 +7,18 @@ export default defineConfig([
     files: ["**/*.{js,mjs,cjs}"],
     plugins: { js },
     extends: ["js/recommended"],
-    languageOptions: { globals: globals.browser },
+    languageOptions: { globals: globals.node },
     rules: {
+      'no-console': ['error', { allow: ['warn', 'error'] }],
       "no-unused-vars": [
         "error",
         {
-          argsIgnorePattern: "^_",
+          argsIgnorePattern: "^_|^(request|reply)$",
           varsIgnorePattern: "^_",
           caughtErrorsIgnorePattern: "^_",
         },
       ],
+      'eol-last': ['error', 'always'],
     },
   },
 ]);
