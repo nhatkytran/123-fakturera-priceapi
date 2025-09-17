95dec1cb4336c29a3c7fa5a7f06ca576a870d477
kytran-saritasa <ky.tran@saritasa.com>
2025-09-17 17:02:58 +0700

    Set up supabase env variables

--- src/config/env.js
+++ src/config/env.js
@@ -2,11 +2,25 @@ import fastifyEnv from '@fastify/env';
 
 const schema = {
   type: 'object',
-  required: ['PORT', 'CORS_ORIGIN_DEV', 'CORS_ORIGIN_PROD'],
+  required: [
+    'PORT',
+    'CORS_ORIGIN_DEV',
+    'CORS_ORIGIN_PROD',
+    'SUPABASE_HOST',
+    'SUPABASE_DB',
+    'SUPABASE_USER',
+    'SUPABASE_PASSWORD',
+    'SUPABASE_PORT',
+  ],
   properties: {
     PORT: { type: 'number', default: 3000 },
     CORS_ORIGIN_DEV: { type: 'string' },
     CORS_ORIGIN_PROD: { type: 'string' },
+    SUPABASE_HOST: { type: 'string' },
+    SUPABASE_DB: { type: 'string' },
+    SUPABASE_USER: { type: 'string' },
+    SUPABASE_PASSWORD: { type: 'string' },
+    SUPABASE_PORT: { type: 'number' },
   },
 };
 
