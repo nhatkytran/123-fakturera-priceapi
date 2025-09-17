7e9f7b7c9e00d1e458b73c9475af00e006281ee1
kytran-saritasa <ky.tran@saritasa.com>
2025-09-17 21:47:18 +0700

    Improve cors

--- src/config/cors.js
+++ src/config/cors.js
@@ -1,13 +1,14 @@
 import fastifyCors from '@fastify/cors';
 
-export const corsOptions = {
-  origin:
-    process.env.NODE_ENV === 'development'
-      ? [process.env.CORS_ORIGIN_DEV, 'http://127.0.0.1:3000/']
-      : process.env.CORS_ORIGIN_PROD,
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  credentials: true,
-};
-
 /** Register cors config. */
-export const registerCors = async fastify => await fastify.register(fastifyCors, corsOptions);
+export const registerCors = async fastify => {
+  const corsOptions = {
+    origin:
+      process.env.NODE_ENV === 'development'
+        ? [fastify.config.CORS_ORIGIN_DEV, 'http://127.0.0.1:3000']
+        : fastify.config.CORS_ORIGIN_PROD,
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true,
+  };
+  await fastify.register(fastifyCors, corsOptions);
+};
