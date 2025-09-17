f67df5154214594d868d1708007609eba2b7067a
kytran-saritasa <ky.tran@saritasa.com>
2025-09-17 11:58:30 +0700

    Add eslint jsdoc

--- eslint.config.js
+++ eslint.config.js
@@ -1,11 +1,12 @@
 import js from "@eslint/js";
 import globals from "globals";
 import { defineConfig } from "eslint/config";
+import jsdoc from 'eslint-plugin-jsdoc';
 
 export default defineConfig([
   {
     files: ["**/*.{js,mjs,cjs}"],
-    plugins: { js },
+    plugins: { js, jsdoc },
     extends: ["js/recommended"],
     languageOptions: { globals: globals.node },
     rules: {
@@ -19,6 +20,21 @@ export default defineConfig([
         },
       ],
       'eol-last': ['error', 'always'],
+      'jsdoc/require-jsdoc': [
+        'warn',
+        {
+          require: {
+            FunctionDeclaration: true,
+            MethodDefinition: true,
+            ClassDeclaration: true,
+            ArrowFunctionExpression: true,
+            FunctionExpression: true,
+          },
+        },
+      ],
+      'jsdoc/check-alignment': 'warn',
+      'jsdoc/check-tag-names': 'warn',
+      'jsdoc/check-param-names': 'warn',
     },
   },
 ]);
