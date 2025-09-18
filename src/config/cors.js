b98a90bde948d3f844bb60a99fe659b726189291
kytran-saritasa <ky.tran@saritasa.com>
2025-09-18 21:05:43 +0700

    Support cors patch method

--- src/config/cors.js
+++ src/config/cors.js
@@ -7,7 +7,7 @@ export const registerCors = async fastify => {
       process.env.NODE_ENV === 'development'
         ? [fastify.config.CORS_ORIGIN_DEV, 'http://127.0.0.1:3000']
         : fastify.config.CORS_ORIGIN_PROD,
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
     credentials: true,
   };
   await fastify.register(fastifyCors, corsOptions);
